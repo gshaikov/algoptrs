c9d62aaeee07d84c6083b73ce6aa7a7abe5721e5
George Shaikouski <george.shaikouski@mobilityhouse.com>
2019-11-01 23:26:28 +0100

    bracketing: hide internals and expose generic API

--- src/bin/bracketing.rs
+++ src/bin/bracketing.rs
@@ -1,7 +1,6 @@
-use algopt::bracketing::bracket::Bracket;
-use algopt::bracketing::golden_section;
+use algopt::bracketing;
 
 fn main() {
     let func = |x: f64| f64::exp(x) * f64::sin(x / 4.0 + x * x);
-    dbg!(golden_section::search(&Bracket::find(func).unwrap()));
+    dbg!(bracketing::golden_section_search(func));
 }
--- src/bracketing.rs
+++ src/bracketing.rs
@@ -1,2 +1,12 @@
-pub mod bracket;
-pub mod golden_section;
+mod bracket;
+mod golden_section;
+
+use crate::Infeasible;
+use bracket::Bracket;
+use golden_section::Point;
+
+pub fn golden_section_search(func: fn(f64) -> f64) -> Result<Point, Infeasible> {
+    let bracket = Bracket::find(func)?;
+    let solution = golden_section::search(&bracket);
+    Ok(solution)
+}
